47343280ad3b407e0bd8d49c19a277b79a791644
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-10-22 20:00:21 -0300

    22/10

--- graficos/infoGlobais.js
+++ graficos/infoGlobais.js
@@ -1,8 +1,9 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
 }
+
 vizualizarInformacoesGlobais()
