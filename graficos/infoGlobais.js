84ce632a854bf97407694af6bcfc3d3a4342046c
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-10-22 21:16:44 -0300

    22/10

--- graficos/infoGlobais.js
+++ graficos/infoGlobais.js
@@ -6,7 +6,13 @@ async function vizualizarInformacoesGlobais() {
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    paragrafo.innerHTML = `No mundo com 7,88 bilhões de pessoas, cerca de 3 bilhões fazem parte da 
+                força de trabalho global, e 2 bilhões delas dependem de transporte para 
+                chegar ao trabalho. Isso significa que, apenas 38% (=3/7,888) no mundo tem 
+                trabalho. Do total de pessoas no mundo 25% utilizam transporte e dos que 
+                trabalham 67% precisam de transporte. E dos que utilizam transporte, estes 
+                gastam 3,75% (0,9/24) do tempo do dia para deslocamento. Esse tempo, embora
+                 pareça curto, simboliza a resiliência e busca por melhores oportunidades.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
