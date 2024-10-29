55f6265e54b039649eab1b99451f47923695c69b
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-10-29 20:49:09 -0300

    29/10

--- graficos/infoGlobais.js
+++ graficos/infoGlobais.js
@@ -6,13 +6,13 @@ async function vizualizarInformacoesGlobais() {
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `No mundo com <span>${total_pessoas_mundo}</span> bilhões de pessoas, 
-                cerca de <span>${total_trabalhadores_mundo}</span> bilhões fazem parte da força de trabalho global, 
-                e <span>${total_pessoas_que_necessitam_transporte_para_trabalho}</span> bilhões 
+    paragrafo.innerHTML = `No mundo com <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas, 
+                cerca de <span>${dados.total_trabalhadores_mundo}</span> bilhões fazem parte da força de trabalho global, 
+                e <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho}</span> bilhões 
                 delas dependem de transporte para chegar ao trabalho. Isso significa que, apenas 38% (=3/7,888) no mundo tem 
                 trabalho. Do total de pessoas no mundo 25% utilizam transporte e dos que 
                 trabalham 67% precisam de transporte. E dos que utilizam transporte, estes 
-                gastam <span>${tempo_medio_deslocamento_para_trabalho}</span> do tempo do dia para deslocamento. Esse tempo, embora
+                gastam <span>${dados.tempo_medio_deslocamento_para_trabalho}</span> do tempo do dia para deslocamento. Esse tempo, embora
                  pareça curto, simboliza a resiliência e busca por melhores oportunidades.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
