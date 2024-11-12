6b8a227d2e37fe4c6acfd8837c27d97f32bbdfb2
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-12 20:35:34 -0300

    12/11

--- graficos/infoGlobais.js
+++ graficos/infoGlobais.js
@@ -3,10 +3,10 @@ const url = 'https://raw.githubusercontent.com/TamirisCeolin/Ciencia-de-Dados-Tu
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json();
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9);
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9);
     const trabalhadoresNoMundo = (dados.total_trabalhadores_mundo / 1e9);
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9);
-    const horas = parseInt(dados.tempo_medio);
+        const horas = parseInt(dados.tempo_medio);
     const minutos = Math.round((dados.tempo_medio - horas) * 100);
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
 
