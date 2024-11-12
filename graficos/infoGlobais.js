bf54836e92ab12db118e2f8a368026f20b33111a
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-12 20:14:20 -0300

    12/11

--- graficos/infoGlobais.js
+++ graficos/infoGlobais.js
@@ -3,7 +3,17 @@ const url='https://raw.githubusercontent.com/TamirisCeolin/Ciencia-de-Dados-Turm
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+    
+    const pessoasMundo = (dados.total_pessoas_mundo / 1e9)
+    const totalTrabalhadoresMundo = (dados.total_trabalhadores_mundo / 1e9)
+    const pessoasQueNecessitamTransporteParaTrabalho = (dados.total_pessoas_que_necessitam_transporte_para_trabalho / 1e9)
+    const horas = parseInt(dados.tempo_medio_deslocamento_para_trabalho)
+    const minutos = Math.round((dados.tempo_medio_deslocamento_para_trabalho - horas) * 60)
+    
+    const pessoasComEducacaoSuperior = (dados.total_pessoas_com_educacao_superior / 1e9)
+
+    const percentual = ((pessoasComAcessoAEducacao/pessoasMundo)*100).toFixed(2)
+    
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `No mundo com <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas, 
@@ -16,6 +26,12 @@ async function vizualizarInformacoesGlobais() {
                  pareça curto, simboliza a resiliência e busca por melhores oportunidades.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
+
+
+  
+    paragrafo.innerHTML =  `Você sabia que o total total de pessoas no mundo é de <span>${pessoasMundo}</span> bilhões de pessoas e que o  total de pessoas com acesso à educação é de <span>${pessoasComAcessoAEducacao}</span> bilhões?  O tempo médio gasto por dia estudando é de <span>${horas} horas </span> e <span>${minutos} minutos</span>  e o total de pessoas com educação superior é de <span>${pessoasComEducacaoSuperior}</span> bilhões. Isso significa que aproximadamente <span>${percentual}%</span> de pessoas tem acesso à Educação. <br>Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?` 
+  
+
 }
 
 vizualizarInformacoesGlobais()
