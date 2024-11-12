97aacedd18435275efde3197bc370364f49d7b65
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-12 20:22:56 -0300

    12/11

--- graficos/infoGlobais.js
+++ graficos/infoGlobais.js
@@ -1,21 +1,23 @@
 const url='https://raw.githubusercontent.com/TamirisCeolin/Ciencia-de-Dados-TurmaHN/refs/heads/main/transpostes-dados-globais.json'
 
-async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    
-    const pessoasMundo = (dados.total_pessoas_mundo / 1e9)
-    const totalTrabalhadoresMundo = (dados.total_trabalhadores_mundo / 1e9)
-    const pessoasQueNecessitamTransporteParaTrabalho = (dados.total_pessoas_que_necessitam_transporte_para_trabalho / 1e9)
-    const horas = parseInt(dados.tempo_medio_deslocamento_para_trabalho)
-    const minutos = Math.round((dados.tempo_medio_deslocamento_para_trabalho - horas) * 60)
-    
-    const pessoasComEducacaoSuperior = (dados.total_pessoas_com_educacao_superior / 1e9)
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url);
+    const dados = await res.json();
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9);
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9);
+    const horas = parseInt(dados.tempo_medio);
+    const minutos = Math.round((dados.tempo_medio - horas) * 100);
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
 
-    const percentual = ((pessoasComAcessoAEducacao/pessoasMundo)*100).toFixed(2)
     
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo} bilhões </span> 
+    de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas} bilhões </span> estão conectadas 
+    em alguma rede social e passam em média <span>${dados.tempo_medio} horas </span> e <span> ${minutos} 
+    minutos </span> conectadas. <br>Isso significa que aproximadamente <span>${porcentagemConectada}% </span> 
+    de pessoas estão conectadas em alguma rede social.`
+
     paragrafo.innerHTML = `No mundo com <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas, 
                 cerca de <span>${dados.total_trabalhadores_mundo}</span> bilhões fazem parte da força de trabalho global, 
                 e <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho}</span> bilhões 
@@ -24,14 +26,10 @@ async function vizualizarInformacoesGlobais() {
                 trabalham 67% precisam de transporte. E dos que utilizam transporte, estes 
                 gastam <span>${dados.tempo_medio_deslocamento_para_trabalho}</span> do tempo do dia para deslocamento. Esse tempo, embora
                  pareça curto, simboliza a resiliência e busca por melhores oportunidades.`
-    const container = document.getElementById('graficos-container')
+
+
+    const container = document.getElementById('graficos-container');
     container.appendChild(paragrafo);
-
-
-  
-    paragrafo.innerHTML =  `Você sabia que o total total de pessoas no mundo é de <span>${pessoasMundo}</span> bilhões de pessoas e que o  total de pessoas com acesso à educação é de <span>${pessoasComAcessoAEducacao}</span> bilhões?  O tempo médio gasto por dia estudando é de <span>${horas} horas </span> e <span>${minutos} minutos</span>  e o total de pessoas com educação superior é de <span>${pessoasComEducacaoSuperior}</span> bilhões. Isso significa que aproximadamente <span>${percentual}%</span> de pessoas tem acesso à Educação. <br>Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?` 
-  
-
 }
 
-vizualizarInformacoesGlobais()
+visualizarInformacoesGlobais();
