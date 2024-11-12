041e2c5ff9a5deb8d4edb61ff0a0b0bd7780b730
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-12 20:29:50 -0300

    12/11

--- graficos/infoGlobais.js
+++ graficos/infoGlobais.js
@@ -4,6 +4,7 @@ async function visualizarInformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json();
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9);
+    const trabalhadoresNoMundo = (dados.total_trabalhadores_mundo / 1e9);
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9);
     const horas = parseInt(dados.tempo_medio);
     const minutos = Math.round((dados.tempo_medio - horas) * 100);
@@ -18,13 +19,13 @@ async function visualizarInformacoesGlobais() {
     minutos </span> conectadas. <br>Isso significa que aproximadamente <span>${porcentagemConectada}% </span> 
     de pessoas estão conectadas em alguma rede social.`
 
-    paragrafo.innerHTML = `No mundo com <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas, 
-                cerca de <span>${dados.total_trabalhadores_mundo}</span> bilhões fazem parte da força de trabalho global, 
-                e <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho}</span> bilhões 
+    paragrafo.innerHTML = `No mundo com <span>${dados.total_pessoas_mundo} bilhões </span>  de pessoas, 
+                cerca de <span>${dados.total_trabalhadores_mundo} bilhões </span> fazem parte da força de trabalho global, 
+                e <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho} bilhões </span>  
                 delas dependem de transporte para chegar ao trabalho. Isso significa que, apenas 38% (=3/7,888) no mundo tem 
                 trabalho. Do total de pessoas no mundo 25% utilizam transporte e dos que 
                 trabalham 67% precisam de transporte. E dos que utilizam transporte, estes 
-                gastam <span>${dados.tempo_medio_deslocamento_para_trabalho}</span> do tempo do dia para deslocamento. Esse tempo, embora
+                gastam <span>${dados.tempo_medio_deslocamento_para_trabalho} horas </span> e <span> ${minutos} minutos </span> do tempo do dia para deslocamento. Esse tempo, embora
                  pareça curto, simboliza a resiliência e busca por melhores oportunidades.`
 
 
