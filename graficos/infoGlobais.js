c132d77fc86563fa631b4fb0534df6f8c275547c
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-12 20:42:05 -0300

    12/11

--- graficos/infoGlobais.js
+++ graficos/infoGlobais.js
@@ -4,7 +4,13 @@ async function visualizarInformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json();
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9);
-  
+    
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9);
+    const trabalhadoresNoMundo = (dados.total_trabalhadores_mundo / 1e9);
+    const horas = parseInt(dados.tempo_medio);
+    const minutos = Math.round((dados.tempo_medio - horas) * 100);
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
+
 
 
     const paragrafo = document.createElement('p');
