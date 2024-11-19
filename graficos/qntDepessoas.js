14d1e517e097553779e35be921becefa384e2a80
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-19 23:48:47 +0000

    19/11

--- graficos/qntDepessoas.js
+++ graficos/qntDepessoas.js
@@ -1,25 +1,63 @@
-async function qntDepessoas() {
-    const url = 'https://raw.githubusercontent.com/TamirisCeolin/Ciencia-de-Dados-TurmaHN/refs/heads/main/transportes-tipos-locomoao.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDoTransporte = Object.keys(dados)
-    const qntDepessoas = Object.values(dados)
+import { getCSS, tickConfig } from "./common.js";
 
-    const data = [
-        {
-          x: nomeDoTransporte,
-          y: qntDepessoas,
-          type: 'bar',
-          marker: {
-            color: getComputedStyle(document.body).getPropertyValue('--secundary-color')
-          }
+async function qntDepessoas() {
+  const url = 'https://raw.githubusercontent.com/TamirisCeolin/Ciencia-de-Dados-TurmaHN/refs/heads/main/transportes-tipos-locomoao.json'
+  const res = await fetch(url)
+  const dados = await res.json()
+  const nomeDoTransporte = Object.keys(dados)
+  const qntDepessoas = Object.values(dados)
+
+  const data = [
+    {
+      x: nomeDoTransporte,
+      y: qntDepessoas,
+      type: 'bar',
+      marker: {
+        color: getCSS('--secundary-color')
+      }
+    }
+  ]
+
+  const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: "Utilização dos meios de transporte no mundo",
+      font: {
+        color: getCSS('--primary-color'),
+        family: getCSS('--font'),
+        size: 30,
+      }
+    },
+    xaxis: {
+      tickfont: tickConfig,
+         title: {
+         text: "Meios de transporte",
+         font: {
+            color: getCSS('--secundary-color'),
+            size: 20,
         }
-      ]
-      
-      const grafico = document.createElement('div')
-      grafico.className = 'grafico'
-      document.getElementById('graficos-container').appendChild(grafico)
-      Plotly.newPlot(grafico, data)
+     }
+   },
+
+   yaxis: {
+    tickfont: tickConfig,
+    title: {
+       text: "Quantidade de pessoas",
+       font: {
+          color: getCSS('--secundary-color'),
+          size: 20,
+      }
+   }
+ }
+
+
+  }
+
+  const grafico = document.createElement('div')
+  grafico.className = 'grafico'
+  document.getElementById('graficos-container').appendChild(grafico)
+  Plotly.newPlot(grafico, data, layout)
 }
 
 qntDepessoas();
